0336fb392fe91cfdcbc13212f97923480d7ffeef
gstark0] <gstark0@icloud.com>
2020-09-20 12:07:20 +0200

    Initial Commit

---
+++ NoteScout/NoteScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteScoutApp.swift
+//  NoteScout
+//
+//  Created by Greg Stark on 20/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct NoteScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
