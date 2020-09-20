a967087476684b5c3476f3f3735962614d8c2d43
gstark0] <gstark0@icloud.com>
2020-09-20 13:14:43 +0200

    Add a button to create new notes

--- NoteScout/ContentView.swift
+++ NoteScout/ContentView.swift
@@ -10,14 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(spacing: 17) {
-                    Note(note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum nisi a urna tincidunt, quis laoreet erat efficitur. Sed vitae tellus aliquet, vehicula libero viverra, elementum sem. Nulla suscipit ex in porttitor laoreet. Morbi iaculis, enim et rutrum semper, tortor est lobortis augue, ut auctor velit diam sed lacus. Curabitur ut enim sit amet mauris rutrum euismod. Sed bibendum interdum molestie. Cras tempus sollicitudin bibendum. Quisque tellus ante, fringilla at mi eu, vulputate euismod eros. Cras pharetra consequat tellus, ut imperdiet tellus condimentum ac. Donec suscipit risus placerat, ultrices neque vel, dictum massa. Integer eros nulla, condimentum pellentesque faucibus sit amet, sodales facilisis nunc")
-                    Note()
-                    Note()
-                    Note()
+            ZStack {
+                ScrollView {
+                    VStack(spacing: 17) {
+                        Note(note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum nisi a urna tincidunt, quis laoreet erat efficitur. Sed vitae tellus aliquet, vehicula libero viverra, elementum sem. Nulla suscipit ex in porttitor laoreet. Morbi iaculis, enim et rutrum semper, tortor est lobortis augue, ut auctor velit diam sed lacus. Curabitur ut enim sit amet mauris rutrum euismod. Sed bibendum interdum molestie. Cras tempus sollicitudin bibendum. Quisque tellus ante, fringilla at mi eu, vulputate euismod eros. Cras pharetra consequat tellus, ut imperdiet tellus condimentum ac. Donec suscipit risus placerat, ultrices neque vel, dictum massa. Integer eros nulla, condimentum pellentesque faucibus sit amet, sodales facilisis nunc")
+                        Note()
+                        Note()
+                        Note()
+                    }
+                    .navigationBarTitle("NoteScout")
                 }
-                .navigationBarTitle("NoteScout")
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("Add note..")
+                        }) {
+                            Image(systemName: "plus")
+                                .font(.system(size: 25))
+                                .foregroundColor(.black)
+                                .padding(25)
+                                .background(Color(.white))
+                                .cornerRadius(50)
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                    .padding(.horizontal, 35)
+                    .padding(.vertical, 20)
+                }
             }
         }
     }
